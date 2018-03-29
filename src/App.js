6f4c83e9dee1c84919d868105cc909caf0ae3b01
Lev <agams92@gmail.com>
2018-03-29 21:44:02 +0300

    Middle of work

--- src/App.js
+++ src/App.js
@@ -6,11 +6,52 @@ let defaultTextColor = '#fff';
 let defaultStyle = {
   color: defaultTextColor
 }
-class Aggregate extends Component {
+let fakeServerData = {
+  user: {
+    name: 'Lev',
+    playlists: [
+      {
+        name: 'My favorites',
+        songs: [ 
+          {name: 'Light my fire', duration: 1345}, 
+          {name: 'You\'ve never give me your money', duration: 1236},
+          {name: 'Sound of silence', duration: 70000}
+        ]
+      },
+      {
+        name: 'Discover Weekly',
+        songs: [
+          {name:'Roll over Beethoven', duration: }, 
+          {name: 'I\'m so keked', duration: },
+          {name: 'Eminence front', duration: }] 
+      },
+      {
+        name: 'Top rap songs',
+        songs: ['Poop', 'Piss', 'I\'m so down'] 
+      },
+      {
+        name: 'Classic',
+        songs: ['Stairway to heaven', 'Smoke on the water', 'When the levee breaks'] 
+      }
+    ]
+  }
+}
+
+class PlaylistCounter extends Component {
   render () {
     return (
       <div style={{...defaultStyle, width: "40%", display: "inline-block"}}>
-        <h2 style ={{color: defaultTextColor}}>Number Text</h2>
+        <h2>{this.props.playlists && this.props.playlists.length} playlists</h2>
+      </div>
+    );
+  }
+} 
+
+class HoursCounter extends Component {
+  render () {
+    return (
+      <div style={{...defaultStyle, width: "40%", display: "inline-block"}}>
+        <h2>{this.props.playlists && this.props.playlists.length} hours</h2>
       </div>
     );
   }
@@ -40,17 +81,32 @@ class Playlist extends Component {
 }
 
 class App extends Component {
+  constructor(){
+    super(...arguments);
+    this.state = {serverData: {}}
+  }
+  componentDidMount () {
+    setTimeout(() => {
+      this.setState({serverData: fakeServerData});
+  }, 1000);
+  }
   render() {
     return (
       <div className="App">
-        <h1>Title</h1>
-          <Aggregate></Aggregate>
-          <Aggregate></Aggregate>
+      {this.state.serverData.user ?
+        <div>
+        {this.state.serverData.user &&
+        <h1>{this.state.serverData.user.name}'s playlist
+        </h1>}
+          <PlaylistCounter playlists = {this.state.serverData.user.playlists}/>
+          <HoursCounter playlists = {this.state.serverData.user.playlists}/>
           <Filter/>
           <Playlist/>
           <Playlist/>
           <Playlist/>
           <Playlist/>
+          </div> : <h1 style = {defaultStyle}>Loading...</h1>
+          }
       </div>
     );
   }
